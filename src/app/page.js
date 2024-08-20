4f70ed424f51def0edfd4d3d7ca8b7c2475af292
Vivek <vivsharma571@gmail.com>
2024-08-20 23:44:48 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Feature from "./components/Feature";
@@ -7,20 +7,20 @@ import Testimonials from "./components/Testimonials";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Top from "./components/Top";
-import LocomotiveScroll from "locomotive-scroll";
+
 
 export default function Home() {
-  const locomotive = new LocomotiveScroll();
+  
   return (
-   <main>
-    <Navbar/>
-    <HeroSection/>
-    <Feature/>
-    <Add/>
-    <Testimonials/>
-    <Content/>
-    <Footer/>
-    <Top/>
-   </main>
+    <main>
+      <Navbar />
+      <HeroSection />
+      <Feature />
+      <Add />
+      <Testimonials />
+      <Content />
+      <Footer />
+      <Top />
+    </main>
   );
 }
